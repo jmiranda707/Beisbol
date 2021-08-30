28d83083b68b699fb107901a1fd5b419cfdc6da9
Jose Miranda <jmiranda@smartdream.es>
2021-08-30 18:01:00 -0430

    listo

--- beisbol/lib/views/partidoPage.dart
+++ beisbol/lib/views/partidoPage.dart
@@ -18,8 +18,8 @@ class PartidoPage extends StatefulWidget {
 }
 
 class _PartidoPageState extends State<PartidoPage> {
-   final CountdownController controller = CountdownController();
-   bool _isPause = true;
+  final CountdownController controller = CountdownController();
+  bool _isPause = true;
   bool _isRestart = false;
 
   @override
@@ -27,12 +27,15 @@ class _PartidoPageState extends State<PartidoPage> {
     Equipo equipo1 = Provider.of<HomeViewModel>(context).equipo1;
     Equipo equipo2 = Provider.of<HomeViewModel>(context).equipo2;
     DatosInning datos = Provider.of<HomeViewModel>(context).datos;
+
+    Equipo equipoAlBate =
+        (datos.abriendoCerrando == 'abriendo') ? equipo1 : equipo2;
     ScreenUtil.init(context,
         width: SizesCustom.width,
         height: SizesCustom.height,
         allowFontScaling: false);
     return Scaffold(
-    /*  appBar: AppBar(
+      /*  appBar: AppBar(
         automaticallyImplyLeading: true,
         backgroundColor: Colorz.rojo,
         title: Row(
@@ -56,87 +59,191 @@ class _PartidoPageState extends State<PartidoPage> {
             height: double.infinity,
             image: AssetImage("assets/wallpapers/resultados.jpeg"),
           ),
-           Positioned(
-             bottom: w(180),
-             left: (MediaQuery.of(context).size.width/2) - w(125),
-                        child: IconRectangularButton(
-                  borderColor: Colorz.blanco,
-                  width: 250,
-                  color: Colorz.verdeOscuro,
-                  function: (){
-                    //Provider.of<HomeViewModel>(context, listen: false).goToPartido(context);
-                  },
-                  height: 60,
-                  nameButton: 'CORRECTO',
-                  icon: Icons.check_circle,
-                  iconColor: Colorz.amarillo,
-                ),
-           ),
-           Positioned(
-             bottom: w(100),
-             left: (MediaQuery.of(context).size.width/2) - w(125),
-                        child: IconRectangularButton(
-                  borderColor: Colorz.blanco,
-                  width: 250,
-                  color: Colorz.rojo,
-                  function: (){
-                    //Provider.of<HomeViewModel>(context, listen: false).goToPartido(context);
-                  },
-                  height: 60,
-                  nameButton: 'INCORRECTO',
-                   icon: Icons.cancel,
-                  iconColor: Colorz.blanco,
-                ),
-           ),
-           Positioned(
-             top: w(50),
-             right: w(80),
-             child: 
-            Countdown(
-      seconds: 20,
-      build: (BuildContext context, double time){
-        final IconData buttonIcon = _isRestart
-        ? Icons.refresh
-        : (_isPause ? Icons.pause : Icons.play_arrow);
-         return Column(
-        children: [
-          Text('Tiempo', style: TextStyle(color: Colorz.blanco, fontSize: f(70), fontWeight: FontWeight.bold)),
-          Text(time.toString(), style: TextStyle(color: Colorz.rojo, fontSize: f(100), fontWeight: FontWeight.bold)),
-          InkWell(
-            onTap: (){
-              final isCompleted = controller.isCompleted;
-          isCompleted ? controller.restart() : controller.pause();
+          Positioned(
+            bottom: w(180),
+            left: (MediaQuery.of(context).size.width / 2) - w(125),
+            child: IconRectangularButton(
+              borderColor: Colorz.blanco,
+              width: 250,
+              color: Colorz.verdeOscuro,
+              function: () {
+                //Provider.of<HomeViewModel>(context, listen: false).goToPartido(context);
+              },
+              height: 60,
+              nameButton: 'CORRECTO',
+              icon: Icons.check_circle,
+              iconColor: Colorz.amarillo,
+            ),
+          ),
+          Positioned(
+            bottom: w(100),
+            left: (MediaQuery.of(context).size.width / 2) - w(125),
+            child: IconRectangularButton(
+              borderColor: Colorz.blanco,
+              width: 250,
+              color: Colorz.rojo,
+              function: () {
+                //Provider.of<HomeViewModel>(context, listen: false).goToPartido(context);
+              },
+              height: 60,
+              nameButton: 'INCORRECTO',
+              icon: Icons.cancel,
+              iconColor: Colorz.blanco,
+            ),
+          ),
+          Positioned(
+            top: w(70),
+            left: w(35),
+            child: _baseWidget(datos.terceraBusy, equipoAlBate.color),
+          ),
+          Positioned(
+            top: w(05),
+            left: w(100),
+            child: _baseWidget(datos.segundaBusy, equipoAlBate.color),
+          ),
+          Positioned(
+            top: w(70),
+            left: w(165),
+            child: _baseWidget(datos.primeraBusy, equipoAlBate.color),
+          ),
+          Positioned(
+            top: w(135),
+            left: w(100),
+            child: _baseWidget(false, equipoAlBate.color),
+          ),
+          Positioned(
+              top: w(50),
+              left: w(320),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _rowInning(datos),
+                  _rowEquipo(equipo1),
+                  _rowEquipo(equipo2),
+                ],
+              )),
+          Positioned(
+              top: w(50),
+              right: w(80),
+              child: Countdown(
+                seconds: 20,
+                build: (BuildContext context, double time) {
+                  final IconData buttonIcon = _isRestart
+                      ? Icons.refresh
+                      : (_isPause ? Icons.pause : Icons.play_arrow);
+                  return Column(
+                    children: [
+                      Text('Tiempo',
+                          style: TextStyle(
+                              color: Colorz.blanco,
+                              fontSize: f(70),
+                              fontWeight: FontWeight.bold)),
+                      Text(time.toString(),
+                          style: TextStyle(
+                              color: Colorz.rojo,
+                              fontSize: f(100),
+                              fontWeight: FontWeight.bold)),
+                      InkWell(
+                          onTap: () {
+                            final isCompleted = controller.isCompleted;
+                            isCompleted
+                                ? controller.restart()
+                                : controller.pause();
 
-          if (!isCompleted && !_isPause) {
-            controller.resume();
-          }
+                            if (!isCompleted && !_isPause) {
+                              controller.resume();
+                            }
 
-          if (isCompleted) {
-            setState(() {
-              _isRestart = false;
-            });
-          } else {
-            setState(() {
-              _isPause = !_isPause;
-            });
-            }
-            },
-            child: Icon(buttonIcon, size: w(100), color: Colorz.amarillo))
+                            if (isCompleted) {
+                              setState(() {
+                                _isRestart = false;
+                              });
+                            } else {
+                              setState(() {
+                                _isPause = !_isPause;
+                              });
+                            }
+                          },
+                          child: Icon(buttonIcon,
+                              size: w(100), color: Colorz.amarillo))
+                    ],
+                  );
+                },
+                controller: controller,
+                interval: Duration(milliseconds: 100),
+                onFinished: () {
+                  print('Tiempo Agotado');
+                  setState(() {
+                    _isRestart = true;
+                  });
+                },
+              ))
         ],
-      );},
-      controller: controller,
-      interval: Duration(milliseconds: 100),
-      onFinished: () {
-        print('Tiempo Agotado');
-        setState(() {
-              _isRestart = true;
-            });
-      },
-    )
-           )
-                               ],
-                             ),
-                           );
-                         }
+      ),
+    );
+  }
 
+  _baseWidget(bool isBusy, Color color) {
+    return new Transform(
+      child: Icon(Icons.stop,
+          size: w(170), color: (isBusy) ? color : Colorz.blanco),
+      alignment: FractionalOffset.center,
+      transform: new Matrix4.identity()..rotateZ(45 * 3.1415927 / 180),
+    );
+  }
+
+  _rowEquipo(Equipo equipo) {
+    return Container(
+      color: equipo.color.withOpacity(0.7),
+      child: Row(
+        children: [
+          Container(
+              width: w(240),
+              child: Text(equipo.nombre,
+                  style: TextStyle(
+                      color: Colorz.blanco,
+                      fontSize: f(40),
+                      fontWeight: FontWeight.bold))),
+          SizedBox(width: w(10)),
+          Container(
+            color: equipo.color,
+            child: Text(equipo.carreras.toString(),
+                style: TextStyle(
+                    color: Colorz.blanco,
+                    fontSize: f(50),
+                    fontWeight: FontWeight.bold)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _rowInning(DatosInning datos) {
+    return Container(
+      width: w(285),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(width: w(100)),
+          
+          SizedBox(width: w(20)),
+         (datos.outs>=1)? Icon(Icons.sports_baseball, size: w(40), color: Colorz.blanco): Offstage(),
+         (datos.outs>=2)? Icon(Icons.sports_baseball, size: w(40), color: Colorz.blanco): Offstage(),
+         (datos.outs>=3)?  Icon(Icons.sports_baseball, size: w(40), color: Colorz.blanco): Offstage(),
+         Text('${datos.inningActual}',
+              style: TextStyle(
+                  color: Colorz.naranja,
+                  fontSize: f(30),
+                  fontWeight: FontWeight.bold)),
+          Icon(
+              (datos.abriendoCerrando == 'abriendo')
+                  ? Icons.arrow_drop_up
+                  : Icons.arrow_drop_down,
+              size: w(40),
+              color: Colorz.naranja),
+        ],
+      ),
+    );
+  }
 }
